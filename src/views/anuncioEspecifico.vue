8b7997eb7d6f06faba89d599d235268a42a1973c
Inês Reis <40200335@esmad.ipp.pt>
2022-01-11 00:41:11 +0000

    tentativa de inicio da pagina anuncio especifico

--- src/views/anuncioEspecifico.vue
+++ src/views/anuncioEspecifico.vue
@@ -2,131 +2,53 @@
   <div class="addPage">
     <section>
       <b-container>
-        <div class="block-content">
-          <div class="product-info">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="gallery">
-                  <div id="product-preview" class="vanilla-zoom">
-                    <div class="zoomed-image"></div>
-                    <div class="sidebar"></div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6">
+        <div class="firstContent">
+          <div class="adInfo">
+            <b-row>
+              <b-col cols="6">
+                <img
+                  class="adImage"
+                  src="../assets/img/pedroNunes.jpg"
+                  style="width=700px; height: 450px;"
+                />
+              </b-col>
+
+              <b-col cols="6">
                 <div class="info">
-                  <h3>Lorem Ipsum</h3>
-                  <div class="rating">
-                    <img src="assets/img/star.svg" /><img
-                      src="assets/img/star.svg"
-                    /><img src="assets/img/star.svg" /><img
-                      src="assets/img/star-half-empty.svg"
-                    /><img src="assets/img/star-empty.svg" />
+                  <p class="adType">Anúncio do tipo ${}</p>
+                  <h3 class="adTitle">Fotografias</h3>
+                  <div class="secondLine">
+                    <p class="announcerName"></p>
                   </div>
-                  <div class="price">
-                    <h3>$300.00</h3>
-                  </div>
-                  <button class="btn btn-primary" type="button">
-                    <i class="icon-basket"></i>Add to Cart
-                  </button>
-                  <div class="summary">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Donec augue nunc, pretium at augue at, convallis
-                      pellentesque ipsum. Vestibulum diam risus, sagittis at
-                      fringilla at, pulvinar vel risus. Vestibulum dignissim eu
-                      nulla eu imperdiet. Morbi mollis tellus a nunc vestibulum
-                      consequat. Quisque tristique elit et nibh dapibus sodales.
-                      Nam sollicitudin a urna sed iaculis.
+                  <div class="containerFavourites">
+                    <!-- icons -->
+                    <p class="favourites">
+                      <i class="favouritesNumber"> 35 </i> favoritos
                     </p>
                   </div>
+                  <div class="adSummary">
+                    <p>
+                      Tiro fotografias de cenários para qualquer um de vós!
+                      Basta me dizerem o que pretendem e quantas fotos
+                      necessitam, a partir daí agilizamos o processo.
+                    </p>
+                  </div>
+                  <div class="announcerCourse">
+                    <p class="courseWritten">CURSO</p>
+                    <p class="courseName">Fotografia</p>
+                  </div>
+                  <div class="announcerTime">
+                    <p class="timeWritten">HORÁRIO</p>
+                    <p class="timeNeeded">24-50 horas</p>
+                  </div>
+                  <button
+                    class="btn btn-primary btnInterested"
+                    type="button">
+                    Tenho interesse
+                  </button>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div class="product-info">
-            <section class="highlight-phone">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-8">
-                    <div class="intro">
-                      <h2>Highlight</h2>
-                      <p>
-                        Nunc luctus in metus eget fringilla. Aliquam sed justo
-                        ligula. Vestibulum nibh erat, pellentesque ut laoreet
-                        vitae. Aliqua sed justo ligula.
-                      </p>
-                      <a class="btn btn-primary" role="button" href="#"
-                        >Action Button</a
-                      >
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="d-none d-md-block phone-mockup">
-                      <img class="device" src="assets/img/phone.svg" />
-                      <div class="screen"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
-          </div>
-          <div class="clean-related-items">
-            <section class="testimonials-clean">
-              <div class="container">
-                <div class="intro">
-                  <h2 class="text-center">Testimonials</h2>
-                  <p class="text-center">
-                    Our customers love us! Read what they have to say below.
-                    Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque
-                    ut laoreet vitae.
-                  </p>
-                </div>
-                <div class="row people">
-                  <div class="col-md-6 col-lg-4 item">
-                    <div class="box">
-                      <p class="description">
-                        Aenean tortor est, vulputate quis leo in, vehicula
-                        rhoncus lacus. Praesent aliquam in tellus eu gravida.
-                        Aliquam varius finibus est.
-                      </p>
-                    </div>
-                    <div class="author">
-                      <img class="rounded-circle" src="assets/img/1.jpg" />
-                      <h5 class="name">Ben Johnson</h5>
-                      <p class="title">CEO of Company Inc.</p>
-                    </div>
-                  </div>
-                  <div class="col-md-6 col-lg-4 item">
-                    <div class="box">
-                      <p class="description">
-                        Praesent aliquam in tellus eu gravida. Aliquam varius
-                        finibus est, et interdum justo suscipit id.
-                      </p>
-                    </div>
-                    <div class="author">
-                      <img class="rounded-circle" src="assets/img/3.jpg" />
-                      <h5 class="name">Carl Kent</h5>
-                      <p class="title">Founder of Style Co.</p>
-                    </div>
-                  </div>
-                  <div class="col-md-6 col-lg-4 item">
-                    <div class="box">
-                      <p class="description">
-                        Aliquam varius finibus est, et interdum justo suscipit.
-                        Vulputate quis leo in, vehicula rhoncus lacus. Praesent
-                        aliquam in tellus eu.
-                      </p>
-                    </div>
-                    <div class="author">
-                      <img class="rounded-circle" src="assets/img/2.jpg" />
-                      <h5 class="name">Emily Clark</h5>
-                      <p class="title">Owner of Creative Ltd.</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </section>
+              </b-col>
+            </b-row>
           </div>
         </div>
       </b-container>
@@ -138,4 +60,8 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.adInfo{
+  margin-top: 50px;
+  }
+</style>
